5535301ca4cdcf82d948b5d0b3cdbc5c8dd283bf
ana <ana@anniecannons.com>
2023-11-13 12:12:26 -0800

    commit ex2 redo i had the wrong name in the wrong folder i have corrected it now

---
+++ ana-npm-color-picker/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { DefaultColorPicker } from '@thednp/react-color-picker'
+import '@thednp/react-color-picker/style.css'
+
+
+function App() {
+  
+    
+  
+
+
+  return (
+    <div className="App">
+      <DefaultColorPicker value="turquoise" onChange={color => console.log(color)} />;
+    </div>
+  );
+}
+
+export default App;
