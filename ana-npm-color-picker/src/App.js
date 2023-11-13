f29f854158e1640a9dcc05e73211eb0d50b6e2d2
ana <ana@anniecannons.com>
2023-11-13 12:13:53 -0800

    format doc

--- ana-npm-color-picker/src/App.js
+++ ana-npm-color-picker/src/App.js
@@ -1,18 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import { DefaultColorPicker } from '@thednp/react-color-picker'
-import '@thednp/react-color-picker/style.css'
-
+import "./App.css";
+import { DefaultColorPicker } from "@thednp/react-color-picker";
+import "@thednp/react-color-picker/style.css";
 
 function App() {
-  
-    
-  
-
-
   return (
     <div className="App">
-      <DefaultColorPicker value="turquoise" onChange={color => console.log(color)} />;
+      <DefaultColorPicker
+        value="turquoise"
+        onChange={(color) => console.log(color)}
+      />
+      ;
     </div>
   );
 }
